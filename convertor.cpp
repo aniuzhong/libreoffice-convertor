c4db444b604bac1575208fcbecb2e2b8f7b6c490
aniuzhong <zxy_9125@163.com>
2024-05-15 15:56:47 +0800

    Update convertor.cpp (no more killing soffice.bin and add create_from_char_array)

--- convertor.cpp
+++ convertor.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <memory>
 #include <iostream>
 #include <filesystem>
 
@@ -14,6 +15,8 @@
 
 #include <cmdline.h>
 
+#include <Windows.h>
+
 using css::uno::Reference;
 using css::uno::XComponentContext;
 using css::frame::XDesktop;
@@ -22,77 +25,8 @@ using css::uno::Sequence;
 using css::beans::PropertyValue;
 using css::frame::XStorable;
 
-#ifdef _WIN32
-#include <windows.h>
-#include <TlHelp32.h>
-
-int64_t get_pid_by_name(const std::string& name)
-{
-    std::map<std::string, int64_t> m;
-
-    PROCESSENTRY32 entry32;
-    entry32.dwSize = sizeof(entry32);
-
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (snapshot == INVALID_HANDLE_VALUE)
-    {
-        std::cout << "CreateToolhelp32Snapshot failed\n";
-        return -1;
-    }
-
-    BOOL res = Process32First(snapshot, &entry32);
-    while (res)
-    {
-        std::string exe_name = entry32.szExeFile;
-        int64_t pid = entry32.th32ProcessID;
-        m.insert({ exe_name, pid });
-        res = Process32Next(snapshot, &entry32);
-    }
-
-    for (const auto [exe_name, pid] : m)
-        if (exe_name == name)
-            return pid;
-
-    return -1;
-}
-
-void kill_by_pid(int64_t pid)
-{
-    if (pid < 0)
-        return;
-
-    HANDLE process = nullptr;
-    process = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
-    if (process == NULL)
-    {
-        std::cout << "OpenProcess failed\n";
-        return;
-    }
-
-    if (TerminateProcess(process, 0) == 0)
-    {
-        std::cout << "TerminateProcess failed\n";
-        return;
-    }
-}
-#endif
-
-void rm_file_lock(const std::string& infile)
-{
-    std::filesystem::path infile1(infile);
-
-    std::string indir = infile1.parent_path().string();
-    for (const auto& entry : std::filesystem::directory_iterator(indir))
-    {
-        if (entry.path().extension() == ".pptx#" ||
-            entry.path().extension() == ".ppt#")
-        {
-            bool res = std::filesystem::remove(entry.path());
-            if (res)
-                std::cout << "remove file " << entry.path() << std::endl;
-        }
-    }
-}
+void             rm_file_lock(const std::string& infile);
+rtl::OUString    create_from_char_array(const char* char_array);
 
 int main(int argc, char* argv[]) try
 {
@@ -106,20 +40,11 @@ int main(int argc, char* argv[]) try
     
     rm_file_lock(prsr.get<std::string>("infile").c_str());
 
-    auto ustrInputSystemPath = rtl::OUString::createFromAscii(prsr.get<std::string>("infile").c_str());
-    auto ustrOutputSystemPath = rtl::OUString::createFromAscii(prsr.get<std::string>("outfile").c_str());
+    rtl::OUString ustrInputSystemPath = create_from_char_array(prsr.get<std::string>("infile").c_str());
+    rtl::OUString ustrOutputSystemPath = create_from_char_array(prsr.get<std::string>("outfile").c_str());
     rtl::OUString ustrOutputUrl, ustrInputUrl;
     osl::FileBase::getFileURLFromSystemPath(ustrInputSystemPath, ustrInputUrl);
     osl::FileBase::getFileURLFromSystemPath(ustrOutputSystemPath, ustrOutputUrl);
-
-#ifdef _WIN32
-    int64_t soffice_bin_pid = get_pid_by_name("soffice.bin");
-    if (soffice_bin_pid > 0)
-    {
-        std::cout << "try to kill soffice.bin " << soffice_bin_pid << " (once) " << std::endl;
-        kill_by_pid(soffice_bin_pid);
-    }
-#endif
 
     Reference<XComponentContext> xComponentContext = cppu::bootstrap();
     auto xMultiComponentFactory = xComponentContext->getServiceManager();
@@ -150,7 +75,7 @@ int main(int argc, char* argv[]) try
                                             sal_Int32(0),
                                             loadProperties);
 
-    std::cout << ustrInputSystemPath << " has been loaded\n";
+    std::cout << std::filesystem::path(prsr.get<std::string>("infile")) << " has been loaded\n";
 
     Reference<XStorable> xStorable(xComponent, css::uno::UNO_QUERY_THROW);
     Sequence<PropertyValue> storeProps(3);
@@ -163,7 +88,7 @@ int main(int argc, char* argv[]) try
     xStorable->storeToURL(ustrOutputUrl, storeProps);
 
     std::cout << "conversion done\n";
-    std::cout << "file has been written in " << ustrOutputSystemPath << '\n';
+    std::cout << "file has been written in " << std::filesystem::path(prsr.get<std::string>("outfile")) << '\n';
 
     return EXIT_SUCCESS;
 }
@@ -171,4 +96,29 @@ catch (css::uno::Exception& e)
 {
     std::cout << e.Message << std::endl;
     return EXIT_FAILURE;
+}
+
+void rm_file_lock(const std::string& infile)
+{
+    std::filesystem::path infile1(infile);
+
+    std::string indir = infile1.parent_path().string();
+    for (const auto& entry : std::filesystem::directory_iterator(indir))
+    {
+        if (entry.path().extension() == ".pptx#" ||
+            entry.path().extension() == ".ppt#")
+        {
+            bool res = std::filesystem::remove(entry.path());
+            if (res)
+                std::cout << "remove file " << entry.path() << std::endl;
+        }
+    }
+}
+
+rtl::OUString create_from_char_array(const char* char_array)
+{
+    int size = MultiByteToWideChar(CP_ACP, 0, char_array, -1, NULL, 0);
+    auto wchar_arr = std::make_unique<WCHAR[]>(size);
+    MultiByteToWideChar(CP_ACP, 0, char_array, -1, (LPWSTR)wchar_arr.get(), size);
+    return rtl::OUString(wchar_arr.get());
 }
